69c8f44205d94d931eaeeedbec8b30ace976532b
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2022-09-26 21:16:29 +0200

    space for paper

--- HelloWorld/RSA.cpp
+++ HelloWorld/RSA.cpp
@@ -3,7 +3,6 @@
 #include<stdlib.h>
 #include <string.h>
 
-
 inline int generatePrimeNumbers() {
     int primeNumbers[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 
@@ -12,6 +11,8 @@ inline int generatePrimeNumbers() {
     return randomValue;
 }
 
+
+//e and totient must be coprime
 inline int exponent(int totient, int p, int q) {
 
     for (int i = 2; i < totient; i++) {
